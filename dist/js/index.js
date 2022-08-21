e4e3f19612a5be3fd8e2e2c7e1ef48cc6cb58ef6
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2022-08-21 18:02:11 -0300

    fim do layout e adicao das funcoes clear e backspace

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("funcionou");
+const visor = document.querySelector(".visor");
+let operadores = [];
+function addElement(element) {
+    operadores.push(element);
+    visor.innerHTML += `${element}`;
+}
+const limpaElemento = () => {
+    visor.innerHTML = "";
+    operadores.pop();
+    for (let op of operadores) {
+        visor.innerHTML += `${op}`;
+    }
+};
+const limpaVisor = () => {
+    visor.innerHTML = "";
+    operadores = [];
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("funcionou")
+const visor = document.querySelector(".visor") as HTMLBodyElement
+let operadores:string[] = []
+
+function addElement(element: string) {
+    operadores.push(element)
+    visor.innerHTML += `${element}`
+}
+
+const limpaElemento = () => {
+    visor.innerHTML = ""
+    operadores.pop()
+    
+    for (let op of operadores) {
+      visor.innerHTML += `${op}`  
+    }
+}
+
+const limpaVisor = () => {
+    visor.innerHTML = ""
+    operadores = []
+}
